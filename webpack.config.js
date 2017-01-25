389f58f185a016eb9a840e0e94e97ef0bb1cc6b9
reuben <reuben.eggar@me.com>
2017-01-25 13:42:07 +0000

    :construction_worker: ESLint Babel & Webpack

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const webpack = require('webpack')
+
+const devMode = process.env.NODE_ENV !== 'production'
+
+module.exports = {
+  entry: ['./front/index.js'],
+  output: {
+    path: './front/compiled',
+    filename: 'bundle.js'
+  },
+  devtool: devMode ? 'source-map' : undefined,
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        loaders: ['style', `css?${(devMode ? 'localIdentName=[local]-[name]-[hash:base64:10]&' : '')}modules=true`]
+      },
+      {
+        test: /\.css$/,
+        include: /node_modules/,
+        loaders: ['style', 'css']
+      },
+      {
+        test: /\.woff2?$/,
+        loader: 'url?limit=10000'
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        loaders: ['url?limit=10000', 'img?minimize=true']
+      }
+    ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': `"${process.env.NODE_ENV}"`,
+      __DEVTOOLS__: devMode
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin()],
+  resolve: {
+    modulesDirectories: 'node_modules',
+    extensions: ['', '.js', '.css']
+  }
+}
+
+if(!devMode){
+  module.exports.plugins.push(
+  new webpack.optimize.UglifyJsPlugin({
+    minimize: !devMode,
+    output: {
+      comments: devMode
+    }
+  }))
+}
