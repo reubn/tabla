9ef86c68e70d08bddd8b0b56e4b832430e8ec888
reuben <reuben.eggar@me.com>
2017-01-28 13:01:11 +0000

    ❌ Remove Debugging Code

--- front/store/actions/selectElement.js
+++ front/store/actions/selectElement.js
@@ -1,7 +1,6 @@
 import {push} from 'react-router-redux'
 
 function selectElementAction(dispatch, atomicNumber, redirect=true){
-  console.log(...arguments)
   dispatch({type: 'SELECT_ELEMENT', atomicNumber})
   if(redirect) return dispatch(push(`/${atomicNumber}`))
 }
