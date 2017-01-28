0abba386a6c0a5e3ba461388f64375b74fc31727
reuben <reuben.eggar@me.com>
2017-01-28 13:02:31 +0000

    💄 Show Selection on Table

--- front/components/PeriodicTableContainer/PeriodicTable/ElementContainer/Element/index.js
+++ front/components/PeriodicTableContainer/PeriodicTable/ElementContainer/Element/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import classnames from 'classnames'
 
-import {element as elementStyle, symbol as symbolStyle, atomicMass as atomicMassStyle, atomicNumber as atomicNumberStyle,
+import {element as elementStyle, selected as selectedStyle, symbol as symbolStyle, atomicMass as atomicMassStyle, atomicNumber as atomicNumberStyle,
   alkaliMetal, halogen, nonMetal, transitionMetal, nobleGas, postTransitionMetal, metalloid, alkalineEarthMetal, actinoid, lanthanoid, unknown
 } from './style'
 
@@ -19,8 +19,8 @@ const groupColours = {
   unknown
 }
 
-const Element = ({element: {symbol, groupBlock, atomicMass, atomicNumber}, select}) => (
-  <section className={classnames(elementStyle, groupColours[groupBlock] || unknown)} onClick={select}>
+const Element = ({element: {symbol, groupBlock, atomicMass, atomicNumber}, select, selected}) => (
+  <section className={classnames(elementStyle, {[selectedStyle]: selected}, groupColours[groupBlock] || unknown)} onClick={select}>
     <span className={atomicNumberStyle}>{atomicNumber}</span>
     <span className={symbolStyle}>{symbol}</span>
     <span className={atomicMassStyle}>{atomicMass.toPrecision(4)}</span>
