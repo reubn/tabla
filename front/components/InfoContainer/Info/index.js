c6f642a3a92e2116e23b2d9acaaff7b1048ee3d6
reuben <reuben.eggar@me.com>
2017-01-28 15:54:46 +0000

    ✨ Element Info

--- front/components/InfoContainer/Info/index.js
+++ front/components/InfoContainer/Info/index.js
@@ -3,8 +3,8 @@ import classnames from 'classnames'
 
 import {info, open} from './style'
 
-const Info = ({elementSelected}) => (
-  <section className={classnames(info, {[open]: elementSelected})}></section>
+const Info = ({elementSelected, element}) => (
+  <section className={classnames(info, {[open]: elementSelected})}>{JSON.stringify((element))}</section>
 )
 
 export default Info
--- front/components/InfoContainer/index.js
+++ front/components/InfoContainer/index.js
@@ -1,8 +1,27 @@
+import React from 'react'
 import {connect} from 'react-redux'
+
+import elements from '../../../data'
+import selectElementAction from '../../store/actions/selectElement'
 
 import Info from './Info'
 
-const mapStateToProps = ({periodicTable: {selectedElement}}) => ({selectedElement})
-const mapDispatchToProps = {}
+const mapStateToProps = ({periodicTable: {selectedElement}}) => ({elementSelected: selectedElement !== null, element: {...elements[selectedElement], atomicNumber: selectedElement}})
+const mapDispatchToProps = {
+  selectElement: atomicNumber => dispatch => selectElementAction(dispatch, atomicNumber, false)
+}
 
-export default connect(mapStateToProps, mapDispatchToProps)(Info)
+class InfoStateSyncer extends React.Component {
+  componentWillMount(){
+    this.init(this.props)
+  }
+  componentWillReceiveProps(nextProps){
+    if(nextProps.params.atomicNumber !== this.props.params.atomicNumber) this.init(nextProps)
+  }
+  init({params: {atomicNumber}, location: {action}, selectElement}){
+    if(action === 'POP' || action === 'REPLACE') return selectElement(+atomicNumber)
+  }
+  render(){return <Info {...this.props} />}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(InfoStateSyncer)
