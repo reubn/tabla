979ba505ede81896147853caac5a5f5465a6bcab
reuben <reuben.eggar@me.com>
2017-01-31 18:14:39 +0000

    ✨ Electron Animation

--- front/components/InfoContainer/Info/Diagram/Shell/index.js
+++ front/components/InfoContainer/Info/Diagram/Shell/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import Electron from './Electron'
 
-import {shell as shellStyle} from './style'
+import {shell as shellStyle, shellRing} from './style'
 
 const Shell = ({shell, electrons}) => {
   const radius = 200 + (shell * 150)
@@ -12,8 +12,8 @@ const Shell = ({shell, electrons}) => {
   for(let i = 0; i < electrons; i++) electronElements.push(<Electron key={i} radius={radius} angle={electronAngle} i={i} />)
 
   return (
-    <g>
-      <circle r={radius} className={shellStyle} />
+    <g className={shellStyle}>
+      <circle r={radius} className={shellRing} />
       {electronElements}
     </g>
   )
