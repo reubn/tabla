f6b53c2b7d0a37d8d50e19fd4014ecd3aaa5f663
reuben <reuben.eggar@me.com>
2017-02-02 21:36:05 +0000

    🐛 Info Flexbox + Scrolling

--- front/components/InfoContainer/Info/Data/index.js
+++ front/components/InfoContainer/Info/Data/index.js
@@ -3,13 +3,11 @@ import React from 'react'
 import Entry from './Entry'
 
 import schemes from './schemes'
-import {data, scroll} from './style'
+import {data} from './style'
 
 const Data = ({element}) => (
   <section className={data}>
-    <section className={scroll}>
-      {schemes.map((scheme, index) => <Entry key={index} element={element} scheme={scheme} />)}
-    </section>
+    {schemes.map((scheme, index) => <Entry key={index} element={element} scheme={scheme} />)}
   </section>
 )
 
