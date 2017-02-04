26e67daa5ff437be24d9707a4ee982285294b356
reuben <reuben.eggar@me.com>
2017-02-04 19:40:40 +0000

    👷 Minification

--- webpack.config.js
+++ webpack.config.js
@@ -1,108 +1,104 @@
 const webpack = require('webpack')
 
+const BabiliPlugin = require('babili-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-const devMode = process.env.NODE_ENV !== 'production'
-
-module.exports = {
-  entry: ['babel-polyfill', './front/app.js'],
-  output: {
-    path: './front/compiled',
-    filename: 'bundle.js'
-  },
-  devtool: devMode ? 'source-map' : undefined,
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: 'babel-loader',
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            query: {
-              localIdentName: devMode ? '[local]-[name]-[hash:base64:10]' : undefined,
-              modules: true
+module.exports = env => {
+  const devMode = env !== 'production'
+  const config = {
+    entry: ['babel-polyfill', './front/app.js'],
+    output: {
+      path: './front/compiled',
+      filename: 'bundle.js'
+    },
+    devtool: devMode ? 'source-map' : undefined,
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: 'babel-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.css$/,
+          exclude: /node_modules/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              query: {
+                localIdentName: devMode ? '[local]-[name]-[hash:base64:10]' : undefined,
+                modules: true
+              }
             }
-          }
-        ]
-      },
-      {
-        test: /\.css$/,
-        include: /node_modules/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.woff2?$/,
-        use: {
-          loader: 'url-loader',
-          query: {
-            limit: 10000
-          }
-        }
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/,
-        use: [{
-          loader: 'url-loader',
-          query: {
-            limit: 10000
+          ]
+        },
+        {
+          test: /\.css$/,
+          include: /node_modules/,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.woff2?$/,
+          use: {
+            loader: 'url-loader',
+            query: {
+              limit: 10000
+            }
           }
         },
         {
-          loader: 'img-loader',
-          query: {
-            minimize: true
+          test: /\.(jpe?g|png|gif|svg)$/,
+          use: [{
+            loader: 'url-loader',
+            query: {
+              limit: 10000
+            }
+          },
+          {
+            loader: 'img-loader',
+            query: {
+              minimize: true
+            }
           }
+          ]
         }
+      ]
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify(devMode ? 'development' : 'production')
+        },
+        __DEVTOOLS__: devMode
+      }),
+      !devMode ? new BabiliPlugin({deadcode: false}) : () => undefined,
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: './front/index.js',
+        inject: false
+      }),
+      new HtmlWebpackPlugin({
+        filename: '404.html',
+        template: './front/index.js',
+        inject: false
+      })
+    ],
+    devServer: {
+      contentBase: ['./front/compiled', './front/'],
+      compress: true,
+      host: '0.0.0.0',
+      port: 80,
+      historyApiFallback: {
+        rewrites: [
+          {from: /[^/]/, to: 'index.html'}
         ]
       }
-    ]
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': `"${process.env.NODE_ENV}"`,
-      __DEVTOOLS__: devMode
-    }),
-    new HtmlWebpackPlugin({  // Also generate a test.html
-      filename: 'index.html',
-      template: './front/index.js',
-      inject: false
-    }),    // Generates default index.html
-    new HtmlWebpackPlugin({  // Also generate a test.html
-      filename: '404.html',
-      template: './front/index.js',
-      inject: false
-    })
-  ],
-  devServer: {
-    contentBase: ['./front/compiled', './front/'],
-    compress: true,
-    host: '0.0.0.0',
-    port: 80,
-    historyApiFallback: {
-      rewrites: [
-        {from: /[^/]/, to: 'index.html'}
-      ]
+    },
+    resolve: {
+      extensions: ['.js', '.css']
     }
-  },
-  resolve: {
-    extensions: ['.js', '.css']
   }
-}
 
-if(!devMode){
-  module.exports.plugins.push(
-  new webpack.optimize.UglifyJsPlugin({
-    sourceMap: true,
-    minimize: !devMode,
-    output: {
-      comments: devMode
-    }
-  }))
+  return config
 }
