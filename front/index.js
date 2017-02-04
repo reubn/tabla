94710c39bb1ca346e4b9eb85b46ef279e530fb86
reuben <reuben.eggar@me.com>
2017-02-04 20:38:27 +0000

    🎨 Remove Comments

--- front/index.js
+++ front/index.js
@@ -1,13 +1,3 @@
-// export default ({htmlWebpackPlugin: {files: {chunks}}}) =>
-//   `<!DOCTYPE html>
-//   <meta name="apple-mobile-web-app-capable" content="yes">
-//   <title>Tabla</title>
-//   <section id="app"></section>
-//   ${Object.values(chunks).map(({entry}) => `<script src="${entry}"></script>`)}
-//   `
-//   // <% for (var chunk in htmlWebpackPlugin.files.chunks) { %>
-//   // <script src="<%= htmlWebpackPlugin.files.chunks[chunk].entry %>"></script>
-//   // <% } %>
 import React from 'react'
 import {renderToStaticMarkup} from 'react-dom/server'
 
