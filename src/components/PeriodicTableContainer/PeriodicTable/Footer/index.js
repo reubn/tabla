4dfe82848e12fa391d51cf96b9ac4cedf5e428ed
reuben <reuben.eggar@me.com>
2017-03-06 22:39:59 +0000

    🔗 Link

--- src/components/PeriodicTableContainer/PeriodicTable/Footer/index.js
+++ src/components/PeriodicTableContainer/PeriodicTable/Footer/index.js
@@ -1,4 +1,4 @@
 import React from 'react'
 import {footer} from './style'
 
-export default () => <footer className={footer}>♥</footer>
+export default () => <a href="//github.com/reubn/tabla" target="_blank" rel="noopener noreferrer" className={footer}>♥</a>
