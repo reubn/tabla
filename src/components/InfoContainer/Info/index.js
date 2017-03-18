134bd5bad0cb0e40213bdd83682117108c4fff5b
reuben <reuben.eggar@me.com>
2017-03-18 10:28:01 +0000

    👠 Info Animation

--- src/components/InfoContainer/Info/index.js
+++ src/components/InfoContainer/Info/index.js
@@ -1,22 +1,30 @@
 import React from 'react'
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup'
 import classnames from 'classnames'
 
-import {Element} from '../../../elements'
 
 import Diagram from './Diagram'
 import Data from './Data'
 
-import {info, open, header, name as nameStyle, close as closeStyle} from './style'
+import {info, open, header, name as nameStyle, close as closeStyle, enter, enterActive, leave, leaveActive, appear, appearActive} from './style'
 
-const Info = ({elementSelected, close, element={}, _: {name, atomicNumber}=element}) => element instanceof Element ? (
-  <section className={classnames(info, {[open]: elementSelected})}>
-    <header className={header}>
-      <span className={closeStyle} onClick={close}>✕</span>
-      <Diagram element={element} />
-      <a href={`//en.wikipedia.org/wiki/Element_${atomicNumber}`} target="_blank" rel="noopener noreferrer" className={nameStyle}>{name}</a>
-    </header>
-    <Data element={element} />
-  </section>
-) : null
+const Info = ({elementSelected, close, element={}, _: {name, atomicNumber}=element}) => (
+  <CSSTransitionGroup
+    component={props => React.Children.toArray(props.children)[0] || null}
+    transitionName={{enter, enterActive, leave, leaveActive, appear, appearActive}}
+    transitionEnterTimeout={200}
+    transitionLeaveTimeout={200}>
+    {elementSelected ? (
+      <section className={classnames(info, {[open]: elementSelected})} key={elementSelected}>
+        <header className={header}>
+          <span className={closeStyle} onClick={close}>✕</span>
+          <Diagram element={element} />
+          <a href={`//en.wikipedia.org/wiki/Element_${atomicNumber}`} target="_blank" rel="noopener noreferrer" className={nameStyle}>{name}</a>
+        </header>
+        <Data element={element} />
+      </section>
+  ) : null}
+  </CSSTransitionGroup>
+)
 
 export default Info
