923153febca08f55d9412cc29bbc9ae563b20322
reuben <reuben.eggar@me.com>
2017-10-21 23:55:56 +0100

    ✨ Transition Delay Effect

--- src/components/InfoContainer/Info/Diagram/Shell/Electron/index.js
+++ src/components/InfoContainer/Info/Diagram/Shell/Electron/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 import {electron} from './style'
 
 const Electron = ({radius, angle, i}) => (
-  <circle cx={radius * Math.sin(angle * i * (Math.PI / 180))} cy={-radius * Math.cos(angle * i * (Math.PI / 180))} className={electron} />
+  <circle cx={radius * Math.sin(angle * i * (Math.PI / 180))} cy={-radius * Math.cos(angle * i * (Math.PI / 180))} className={electron} style={{transitionDelay: `${0.01 * i}s`}} />
 )
 
 export default Electron
