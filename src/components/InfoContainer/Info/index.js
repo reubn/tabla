41b3b3211c94f47bc524409fd4937cda64a7b50d
reuben <reuben.eggar@me.com>
2017-10-21 23:34:05 +0100

    🐛 New CSSTransition

--- src/components/InfoContainer/Info/index.js
+++ src/components/InfoContainer/Info/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup'
+import {CSSTransition} from 'react-transition-group'
 import classnames from 'classnames'
 
 
 import Diagram from './Diagram'
 import Data from './Data'
 
-import {info, open, header, name as nameStyle, close as closeStyle, enter, enterActive, leave, leaveActive, appear, appearActive,
-        alkaliMetal, halogen, nonMetal, transitionMetal, nobleGas, postTransitionMetal, metalloid, alkalineEarthMetal, actinoid, lanthanoid, unknown} from './style'
+import {info, open, header, name as nameStyle, close as closeStyle, enter, enterActive, exit, exitActive, appear, appearActive,
+  alkaliMetal, halogen, nonMetal, transitionMetal, nobleGas, postTransitionMetal, metalloid, alkalineEarthMetal, actinoid, lanthanoid, unknown} from './style'
 
 const groupColours = {
   alkaliMetal,
@@ -24,22 +24,20 @@ const groupColours = {
 }
 
 const Info = ({elementSelected, close, element={}, _: {name, atomicNumber, groupBlock}=element}) => (
-  <CSSTransitionGroup
-    component={props => React.Children.toArray(props.children)[0] || null}
-    transitionName={{enter, enterActive, leave, leaveActive, appear, appearActive}}
-    transitionEnterTimeout={200}
-    transitionLeaveTimeout={200}>
-    {elementSelected ? (
-      <section className={classnames(info, {[open]: elementSelected}, groupColours[groupBlock] || unknown)} key={elementSelected}>
-        <header className={header}>
-          <span className={closeStyle} onClick={close}>✕</span>
-          <Diagram element={element} />
-          <a href={`//en.wikipedia.org/wiki/Element_${atomicNumber}`} target="_blank" rel="noopener noreferrer" className={nameStyle}>{name}</a>
-        </header>
-        <Data element={element} />
-      </section>
-  ) : null}
-  </CSSTransitionGroup>
+  <CSSTransition
+    in={elementSelected}
+    classNames={{enter, enterActive, exit, exitActive, appear, appearActive}}
+    timeout={200}
+  >
+    <section className={classnames(info, {[open]: elementSelected}, groupColours[groupBlock] || unknown)}>
+      <header className={header}>
+        <span className={closeStyle} onClick={close}>✕</span>
+        {elementSelected && <Diagram element={element} />}
+        <a href={`//en.wikipedia.org/wiki/Element_${atomicNumber}`} target="_blank" rel="noopener noreferrer" className={nameStyle}>{name}</a>
+      </header>
+      <Data element={element} />
+    </section>
+  </CSSTransition>
 )
 
 export default Info
