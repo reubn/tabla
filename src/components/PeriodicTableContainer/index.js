2420d0d94b72682175cece720b18e495ef541bdb
reuben <reuben.eggar@me.com>
2017-10-24 13:16:44 +0100

    ✨ Provide selectedElement to PeriodicTable

--- src/components/PeriodicTableContainer/index.js
+++ src/components/PeriodicTableContainer/index.js
@@ -2,7 +2,7 @@ import {connect} from 'react-redux'
 
 import PeriodicTable from './PeriodicTable'
 
-const mapStateToProps = ({periodicTable: {visibleElements}}) => ({visibleElements})
+const mapStateToProps = ({periodicTable: {visibleElements, selectedElement}}) => ({visibleElements, selectedElement})
 const mapDispatchToProps = {}
 
 export default connect(mapStateToProps, mapDispatchToProps)(PeriodicTable)
