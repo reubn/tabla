e9f1a13f946f0ed18e2890457c03e9009db14b6b
reuben <reuben.eggar@me.com>
2017-11-19 10:24:07 +0000

    ✨ Async selectElement on Server

--- src/store/actions/selectElement.js
+++ src/store/actions/selectElement.js
@@ -1,6 +1,8 @@
 import {fullElement} from '../../elements'
 
 export default async (dispatch, atomicNumber, triggerRedirect=true) => {
+  if(typeof window !== 'object') return dispatch({type: 'SELECT_ELEMENT', triggerRedirect, atomicNumber})
+
   await fullElement(atomicNumber)
   return dispatch({type: 'SELECT_ELEMENT', triggerRedirect, atomicNumber})
 }
