724ad2d3c7ef84f594679810e6a66503283ec209
reuben <reuben.eggar@me.com>
2017-11-19 10:22:34 +0000

    ✨ Fetch JSON on Client

--- src/elements/index.js
+++ src/elements/index.js
@@ -1,13 +1,17 @@
 import FullElement from './FullElement'
 
+const fullElementsCache = {}
 export const fullElement = async atomicNumber => {
   if(!atomicNumber) return {}
 
-  const json = typeof window === 'object'
-    ? import(/* webpackChunkName: "fullElements" */`../../data/dist/${atomicNumber}`)
-    : __non_webpack_require__(`../../data/dist/${atomicNumber}`) // eslint-disable-line no-undef
+  if(fullElementsCache[atomicNumber]) return fullElementsCache[atomicNumber]
 
-  return new FullElement(atomicNumber, await json)
+  const json = typeof window === 'object'
+    : __non_webpack_require__(`../../data/dist/${atomicNumber}`) // eslint-disable-line no-undef
+    ? (await fetch(`/${atomicNumber}.json`)).json()
+
+  fullElementsCache[atomicNumber] = new FullElement(atomicNumber, await json)
+  return fullElementsCache[atomicNumber]
 }
 
 export {default as layout} from './layout'
