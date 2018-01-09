358ee7e4f0fbef975c275556eff0235d1c2786ae
reuben <reuben.eggar@me.com>
2018-01-09 12:22:14 +0000

    ⚡️ Save Bytes

--- src/components/InfoContainer/Info/Diagram/Shell/Electron/index.js
+++ src/components/InfoContainer/Info/Diagram/Shell/Electron/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 import {electron} from './style'
 
 const Electron = ({radius, angle, i}) => (
-  <circle cx={radius * Math.sin(angle * i * (Math.PI / 180))} cy={-radius * Math.cos(angle * i * (Math.PI / 180))} className={electron} style={{transitionDelay: `${0.01 * i}s`}} />
+  <circle cx={(radius * Math.sin(angle * i * (Math.PI / 180))).toFixed(2)} cy={(-radius * Math.cos(angle * i * (Math.PI / 180))).toFixed(2)} className={electron} style={{transitionDelay: `${0.01 * i}s`}} />
 )
 
 export default Electron
