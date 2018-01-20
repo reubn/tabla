378e3b0ea124ceca0eae613d6ac351a12bf46175
reuben <reuben.eggar@me.com>
2018-01-20 14:50:32 +0000

    ✨ Remove Link from Logo

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,13 +3,16 @@ import React from 'react'
 import PeriodicTableContainer from '../PeriodicTableContainer'
 import InfoContainer from '../InfoContainer'
 import AboutContainer from '../AboutContainer'
+import Link from '../Link'
 import Logo from '../Logo'
 
 import {app} from './style'
 
 const App = () => (
   <section className={app}>
-    <Logo />
+    <Link to="/about">
+      <Logo />
+    </Link>
     <PeriodicTableContainer />
     <InfoContainer />
     <AboutContainer />
--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -2,12 +2,9 @@ import React from 'react'
 
 import classnames from 'classnames'
 
-import Link from '../Link'
-
 import {logo, dot, cross, lightBackground, darkBackground} from './style'
 
 const Logo = ({light=false, dotClassname=classnames(dot, light ? lightBackground : darkBackground), crossClassname=classnames(cross, light ? lightBackground : darkBackground)}) => (
-  <Link to="/">
     <svg viewBox="0 0 264 264" className={logo}>
       <rect className={dotClassname} transform="rotate(45 132 35.833)" x="107" y="10.833" width="50" />
       <rect className={dotClassname} transform="rotate(45 228.167 132)" x="203.167" y="107" width="50" />
@@ -20,7 +17,6 @@ const Logo = ({light=false, dotClassname=classnames(dot, light ? lightBackground
       <rect className={crossClassname} transform="rotate(135 66.24 197.76)" x="16.239" y="172.761" width="100" />
       <rect className={crossClassname} transform="rotate(135 197.76 66.24)" x="147.761" y="41.239" width="100" />
     </svg>
-  </Link>
 )
 
 export default Logo
