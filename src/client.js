35e93327df1b3db8d6ae21146d5eb0ec3e67847a
reuben <reuben.eggar@me.com>
2018-01-20 16:19:32 +0000

    🔥

--- src/client.js
+++ src/client.js
@@ -1,5 +1,3 @@
-/* global __BUILD__:true */
-
 import React from 'react'
 import {hydrate} from 'react-dom'
 
@@ -7,8 +5,6 @@ import store from './store'
 import {linkHistoryToStore} from './routing'
 
 import Root from './components/Root'
-
-window.build = __BUILD__
 
 linkHistoryToStore(store)
 
