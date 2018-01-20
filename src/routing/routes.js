8f7483d7c15bc889260f05cc4b99dd76dc7a35b6
reuben <reuben.eggar@me.com>
2018-01-20 14:37:27 +0000

    🍭

--- src/routing/routes.js
+++ src/routing/routes.js
@@ -5,7 +5,7 @@ export default [
     path: '/about',
     action: 'TOGGLE_ABOUT',
     actionCreator: (getState, dispatch) => dispatch({type: 'TOGGLE_ABOUT', payload: true, triggerRedirect: false}),
-    pathCreator: ({payload}) => `/${payload ? 'about' : ''}` // Creates path when action matches
+    pathCreator: ({payload}) => `/${payload ? 'about' : ''}`
   },
   {
     path: '/:atomicNumber',
