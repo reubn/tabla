0bd856dbb670a6f36d47bd359bccf632ba2fd308
reuben <reuben.eggar@me.com>
2018-01-26 13:08:54 +0000

    ✨ About

--- src/components/AboutContainer/About/index.js
+++ src/components/AboutContainer/About/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import classnames from 'classnames'
 
+import Close from '../../Close'
 import Logo from '../../Logo'
 
-import {about, open} from './style'
+import {about, open, logo, build} from './style'
 
-export default ({visible}) => (
-  <section className={classnames(about, {[open]: visible})}><Logo light/></section>
+export default ({visible, close}) => (
+  <section className={classnames(about, {[open]: visible})}>
+    <Close onClick={close} />
+    <Logo className={logo} light/>
+    <span className={build}>{typeof window === 'object' ? window.build : global.build}</span>
+  </section>
 )
--- src/components/AboutContainer/index.js
+++ src/components/AboutContainer/index.js
@@ -4,7 +4,7 @@ import About from './About'
 
 const mapStateToProps = ({about}) => ({visible: about})
 const mapDispatchToProps = {
-  close: () => dispatch => selectElementAction(dispatch, null)
+  close: () => dispatch => dispatch({type: 'TOGGLE_ABOUT', triggerRedirect: true})
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(About)
