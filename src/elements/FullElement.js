1c4f9cf8915f37a3b3806ab37eed47a6fb2063ba
reuben <reuben.eggar@me.com>
2018-03-24 10:53:56 +0000

    🙌🏼

--- src/elements/FullElement.js
+++ src/elements/FullElement.js
@@ -8,16 +8,48 @@ export default class FullElement extends BasicElement {
     this.period = json.p
     this.block = json.b
     this.uses = json.u
-    this.electronAffinity = json.ea
-    this.covalentRadius = json.cr
     this.ionisationEnergies = json.ies
     this.cas = json.cas
-    this.electronegativity = json.en
-    this.atomicRadius = json.ar
     this.oxidationStates = json.os
     this.meltingPoint = json.mp
     this.boilingPoint = json.bp
-    this.density = json.dp
+    this.density = json.d
+
+    this.evaporationHeat = json.eh
+    this.fusionHeat = json.fh
+    this.specificHeat = json.sph
+    this.heatOfFormation = json.hof
+    this.thermalConductivity = json.tc
+    this.atomicRadius = json.ar
+    this.atomicVolume = json.av
+    this.metallicRadius = json.mr
+    this.vanDerWaalsRadius = json.vdwr
+    this.covalentRadius = json.cr
+    this.covalentRadiusDouble = json.crd
+    this.covalentRadiusTriple = json.crt
+    this.gasBasicity = json.gbas
+    this.dipolePolarisability = json.dp
+    this.electronAffinity = json.ea
+    this.protonAffinity = json.pa
+    this.electronegativity = json.en
+    this.latticeConstant = json.lc
+    this.latticeStructure = json.ls
+    this.c6 = json.c6
+
+    this.uses = json.uses
+    this.sources = json.sour
+    this.description = json.desc
+    this.nameOrigin = json.no
+    this.discoverers = json.disc
+    this.discoveryLocation = json.discLoc
+    this.discoveryYear = json.discYear
+
+    this.annotations = json.ann
+
+    this.abundance = (json.ca || json.sa) ? {
+      crust: json.ca,
+      sea: json.sa
+    } : undefined
 
     this.isotopes = json.i ? json.i.map(({n, a, r, hl: {t, u}={}}) => ({
       neutrons: n,
