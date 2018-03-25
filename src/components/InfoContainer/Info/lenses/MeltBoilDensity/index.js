82e4aeda643fcc3d21d87b60a7be2ffea7d76685
reuben <reuben.eggar@me.com>
2018-03-25 23:11:28 +0100

    ✨ MeltBoilDensity

---
+++ src/components/InfoContainer/Info/lenses/MeltBoilDensity/index.js
@@ -0,0 +1,85 @@
+import React, {Component} from 'react'
+import classnames from 'classnames'
+import sigdig from 'sigdig'
+
+import {bestElement} from '../../../../../elements'
+
+import {lens, property, label, figure, sup, inline  } from './style'
+
+export default class MeltBoilDensityLens extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      full: false
+    }
+  }
+
+  static test(element){
+    return !!element.meltingPoint || !!element.boilingPoint || !!element.density
+  }
+
+  setFull(full=!this.state.full){
+    this.setState(() => ({full: full}))
+  }
+
+  render(){
+    return (
+      <section className={lens}>
+        <section className={property}>
+          <label className={label}>Melting Point</label>
+          <span className={figure}>
+            {sigdig(this.props.element.meltingPoint - 273, 5)}
+            <span className={sup}>°C</span>
+          </span>
+        </section>
+        <section className={property}>
+          <label className={label}>Boiling Point</label>
+          <span className={figure}>
+            {sigdig(this.props.element.boilingPoint - 273, 5)}
+            <span className={sup}>°C</span>
+          </span>
+        </section>
+        <section className={property}>
+          <label className={label}>Density</label>
+          <span className={figure}>
+            {sigdig(this.props.element.density, 5)}
+            <span className={inline}>g cm⁻³</span>
+          </span>
+        </section>
+      </section>
+    )
+  }
+}
+
+/* <section class="lens-👩🏻&zwj;🔧" style="
+  margin-top: 2rem;
+  ">
+  <section class="electronicConfiguration-🗝">
+  <span class="electronicConfigurationInline-👊" style="
+    display: flex;
+     ">
+     <section class="a"><label class="label-🏃🏼">Melting Point</label><span style="
+       display: flex;
+       justify-content: center;
+        ">116.6<span class="electronsSuperscript-🔗">°C</span></span>
+     </section>  <section class="a"><label class="label-🏃🏼">Boiling Point</label><span>120.85<span class="electronsSuperscript-🔗">°C</span></span>
+     </section><section class="a"><label class="label-🏃🏼">Density</label><span>116.6<span class="" style="
+       vertical-align: middle;
+       font-size: 0.8rem;
+       font-weight: bold;
+       color: var(--colours-grey-4);
+       margin-left: 2px;
+       ">g dm⁻¹</span></span>
+     </section></span></section></section> */
+
+//      .a {
+//     display: flex;
+//     width: 50%;
+//     flex-direction: column;
+//     height: 4rem;
+//     justify-content: center;
+// }
+//
+// .a:not(:last-of-type) {
+//     border-right: solid 1px var(--colours-white-3);
+// }
--- src/components/InfoContainer/Info/lenses/index.js
+++ src/components/InfoContainer/Info/lenses/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 
 import ElectronicLens from './Electronic'
+import MeltBoilDensityLens from './MeltBoilDensity'
 
-const lenses = [ElectronicLens]
+const lenses = [ElectronicLens, MeltBoilDensityLens]
 
 export default element => lenses.reduce((active, Current) => Current.test(element) ? [...active, <Current element={element} key={Current.constructor.name} />] : active, [])
