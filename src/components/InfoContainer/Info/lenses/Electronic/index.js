ee39ac84b2b7e2648a096cee753a6e788003f94e
reuben <reuben.eggar@me.com>
2018-03-25 18:17:07 +0100

    ✨ Toggle Full and Collapsed electronicConfiguration

--- src/components/InfoContainer/Info/lenses/Electronic/index.js
+++ src/components/InfoContainer/Info/lenses/Electronic/index.js
@@ -5,7 +5,7 @@ import {bestElement} from '../../../../../elements'
 
 import Diagram from './Diagram'
 
-import {lens, electronicConfiguration, electronsSuperscript, elementAbbreviation,
+import {lens, electronicConfiguration, full as fullStyle, inside, electronsSuperscript, elementAbbreviation,
         alkaliMetal, halogen, nonMetal, transitionMetal, nobleGas, postTransitionMetal, metalloid, alkalineEarthMetal, actinoid, lanthanoid, unknown} from './style'
 
 const groupColours = {
@@ -23,8 +23,19 @@ const groupColours = {
 }
 
 export default class ElectronicLens extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      full: false
+    }
+  }
+
   static test(element){
     return !!element.electronicConfigurationRaw
+  }
+
+  setFull(full=!this.state.full){
+    this.setState(() => ({full: full}))
   }
 
   render(){
@@ -32,17 +43,18 @@ export default class ElectronicLens extends Component {
       <section className={lens} key={this.props.key}>
         <Diagram element={this.props.element} />
         <section className={electronicConfiguration}>
-          {this.props.element.electronicConfiguration(false, {
+          {this.props.element.electronicConfiguration(this.state.full, {
             element: atomicNumber => (
               <span
-                className={classnames(elementAbbreviation, groupColours[this.props.element.groupBlock])}>
+                className={classnames(elementAbbreviation, groupColours[this.props.element.groupBlock])}
+                onClick={() => this.setFull(true)}>
                 {bestElement(atomicNumber).symbol}
               </span>
             ),
             shell: shell => <span> {shell}</span>,
             subshell: subshell => <span>{subshell}</span>,
             electrons: electrons => <span className={electronsSuperscript}>{electrons}</span>,
-            combine: parts => <span>{parts}</span>
+            combine: parts => <span className={classnames(inside, {[fullStyle]: this.state.full})} onClick={() => this.state.full && this.setFull(false)}>{parts}</span>
         })}
         </section>
       </section>
