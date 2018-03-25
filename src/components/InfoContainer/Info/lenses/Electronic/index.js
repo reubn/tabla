5a4aaa8fffd162bb974d8f78680c605c953bb6dc
reuben <reuben.eggar@me.com>
2018-03-25 13:41:44 +0100

    🙌🏼 Pretty electronicConfiguration

--- src/components/InfoContainer/Info/lenses/Electronic/index.js
+++ src/components/InfoContainer/Info/lenses/Electronic/index.js
@@ -1,19 +1,21 @@
 import React from 'react'
 
+import {bestElement} from '../../../../../elements'
+
 import Lens from '../Lens'
 
 import Diagram from './Diagram'
 
-import {lens, electronicConfiguration, electronsSuperscript} from './style'
+import {lens, electronicConfiguration, electronsSuperscript, elementAbbreviation} from './style'
 
 const lensComponent = ({element, key}) => (
   <section className={lens} key={key}>
     <Diagram element={element} />
     <section className={electronicConfiguration}>{element.electronicConfiguration(false, {
-      element: symbol => symbol,
-      shell: shell => shell,
-      subshell: subshell => subshell,
-        electrons: electrons => <span className={electronsSuperscript}>{electrons}</span>,
+      element: atomicNumber => <span className={elementAbbreviation}>{bestElement(atomicNumber).symbol}</span>,
+      shell: shell => <span> {shell}</span>,
+      subshell: subshell => <span>{subshell}</span>,
+      electrons: electrons => <span className={electronsSuperscript}>{electrons}</span>,
       combine: parts => <span>{parts}</span>
     })}</section>
   </section>
