ca9799f37b8243f6ea036f5780421ce61efaac18
reuben <reuben.eggar@me.com>
2018-03-26 21:46:18 +0100

    ✨ Accept FactoryLenses

--- src/components/InfoContainer/Info/lenses/index.js
+++ src/components/InfoContainer/Info/lenses/index.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 import ElectronicLens from './Electronic'
 import MeltBoilDensityLens from './MeltBoilDensity'
+import TextLens from './Text'
 
-const lenses = [ElectronicLens, MeltBoilDensityLens]
+const lenses = [
+  ElectronicLens,
+  MeltBoilDensityLens,
+  {
+    Lens: TextLens,
+    test: element => !!element.description,
+    props: element => ({
+      label: 'Description',
+      text: element.description
+    })
+  }
+]
 
 export default element => lenses.reduce((active, Current, index) => Current.test(element) ? [...active, <Current element={element} key={index} />] : active, [])
