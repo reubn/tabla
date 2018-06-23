7243fd1bc4f8267869816dae64b2910cc29e3467
Reuben <reuben.eggar@me.com>
2018-06-23 22:12:53 +0100

    ✨ License and Attribution

--- src/components/AboutContainer/About/index.js
+++ src/components/AboutContainer/About/index.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import classnames from 'classnames'
 
+import licenceText from 'raw-loader!../../../../LICENSE'
+
 import Close from '../../Close'
 import Logo from '../../Logo'
+import Link from '../../Link'
 
-import {about, open, logo, build} from './style'
+import {about, open, logo, build, title} from './style'
 
 export default ({visible, close}) => (
   <section className={classnames(about, {[open]: visible})}>
     <Close onClick={close} />
     <Logo className={logo} light/>
+    <p className={title}>A project by <a target="_blank" rel="noopener noreferrer" href="https://reuben.science">Reuben</a></p>
+    <hr />
+    <span>Using data from <a target="_blank" rel="noopener noreferrer" href="https://bitbucket.org/lukaszmentel/mendeleev">mendeleev</a>.</span>
+    <span>{licenceText}</span>
     <span className={build}>{typeof window === 'object' ? window.build : global.build}</span>
   </section>
 )
