6869d54c4976ec6589e50913a2c24c94a1646732
Reuben <4957964+reubn@users.noreply.github.com>
2020-01-18 12:23:50 +0000

    😋 Better Animation

--- src/components/InfoContainer/Info/lenses/ElectronicDiagram/Diagram/Shell/index.js
+++ src/components/InfoContainer/Info/lenses/ElectronicDiagram/Diagram/Shell/index.js
@@ -4,7 +4,7 @@ import Electron from './Electron'
 
 import {shell as shellStyle, shellRing} from './style'
 
-const Shell = ({shell, electrons}) => {
+const Shell = ({shell, electrons, totalShells}) => {
   const radius = 200 + (shell * 150)
 
   const electronAngle = 360 / electrons
@@ -12,7 +12,7 @@ const Shell = ({shell, electrons}) => {
   for(let i = 0; i < electrons; i++) electronElements.push(<Electron key={i} radius={radius} angle={electronAngle} i={i} />)
 
   return (
-    <g className={shellStyle}>
+    <g className={shellStyle} style={{animationDuration: `${60 + (((totalShells - shell) / totalShells) * 120)}s`}}>
       <circle r={radius} className={shellRing} />
       {electronElements}
     </g>
--- src/components/InfoContainer/Info/lenses/ElectronicDiagram/Diagram/index.js
+++ src/components/InfoContainer/Info/lenses/ElectronicDiagram/Diagram/index.js
@@ -8,7 +8,7 @@ import {diagram} from './style'
 const Diagram = ({element, _: {symbol}=element}) => (
   <svg className={diagram} viewBox="-1300 -1300 2600 2600">
     <Nucleus symbol={symbol} />
-    {element.electronsPerShell && Object.entries(element.electronsPerShell()).map(([shell, electrons]) => <Shell key={shell} shell={shell} electrons={electrons} />)}
+    {element.electronsPerShell && Object.entries(element.electronsPerShell()).map(([shell, electrons], _, {length}) => <Shell key={shell} shell={shell} totalShells={length} electrons={electrons} />)}
   </svg>
 )
 
